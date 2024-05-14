84cb6e6019d3c552ca506c928ce6bf7fe9fb35f9
Mina Hany <mina.hanna@itworxedu.com>
2024-05-14 18:31:35 +0300

    Handle NowPlayingVc, NowPlayingVm

--- challenge05/Sources/Presentation/NowPlayingVc/PlayingViewController.swift
+++ challenge05/Sources/Presentation/NowPlayingVc/PlayingViewController.swift
@@ -6,24 +6,80 @@
 //
 
 import UIKit
+import Combine
 
-class PlayingViewController: UIViewController {
-
+class PlayingViewController: BaseViewController {
+    private var cancellabels = Set<AnyCancellable>()
+    var viewModel: PlayingViewModelProtocol!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Now Playing"
+        
+        startLoading()
+        viewModel = PlayingViewModel()
+        viewModel.viewDidLoad()
+        bindData()
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        viewModel.viewDidAppear()
+    }
+}
+
+//MARK: - UITableViewDataSource
+extension PlayingViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.dataSource.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeue(tableViewCell: MovieTableViewCell.self, forIndexPath: indexPath)
+        let movie = viewModel.getMovie(index: indexPath.row)
+        cell.setupCell(movie: movie)
+        return cell
     }
-    */
+}
 
+//MARK: - Data Binding
+extension PlayingViewController {
+    private func bindData() {
+        viewModel.errorPublisher
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] error in
+                if let error = error {
+                    print("Error", error)
+                    self?.stopLoading()
+                }
+            })
+            .store(in: &cancellabels)
+        
+        viewModel.moviesPublisher
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { [weak self] fetched in
+                if fetched ?? false {
+                    self?.stopLoading()
+                    self?.tableView.reloadData()
+                    self?.updateEmptyStateView()
+
+                }
+            })
+            .store(in: &cancellabels)
+    }
+}
+
+//MARK: - UIScrollViewDelegate
+extension PlayingViewController {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y + scrollView.frame.size.height >= scrollView.contentSize.height {
+            if !viewModel.isLoading {
+                // Load more data
+                startLoading()
+                viewModel.fetchMovies()
+            }
+        }
+    }
 }
