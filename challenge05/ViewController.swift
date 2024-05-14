8db10eeed76b00c70f4936f28578463e763641a1
Mina Hany <mina.hanna@itworxedu.com>
2024-05-14 15:45:46 +0300

    starting project

---
+++ challenge05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge05
+//
+//  Created by Mina Hanna on 2024-05-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
